a6e676367919e0401b1d29d446aa6e0c779a269f
Pinny Geliebter <pgibralter@gmail.com>
2021-10-18 18:51:18 -0400

    turns off ssr and static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
-	ssr: false,
-	target: 'static',
+	// ssr: false,
+	// target: 'static',
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
 		title: 'crypto-dreams',
