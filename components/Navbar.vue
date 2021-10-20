611c91301356cb2ed447590a0e326d833b1e236e
Pinny Geliebter <pgibralter@gmail.com>
2021-10-19 20:56:25 -0400

    creates navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,97 @@
+<template>
+  <!--//page-header//-->
+  <header class="navbar py-0 page-header navbar-expand navbar-light px-4">
+    <ul class="navbar-nav d-flex align-items-center h-100">
+      <!-- this is the four squares which will be used for navigation -->
+      <li
+        class="
+          nav-item
+          d-flex
+          flex-column
+          h-100
+          justify-content-center
+          dropdown
+        "
+      >
+        <a
+          href="#"
+          data-bs-toggle="dropdown"
+          class="
+            hover-bg-primary
+            nav-link
+            rounded-circle
+            size-35
+            d-flex
+            align-items-center
+            justify-content-center
+            p-0
+          "
+        >
+          <i data-feather="grid" class="fe-1x align-middle"></i>
+        </a>
+        <div class="dropdown-menu overflow-hidden p-0 mt-0">
+          <div class="row mx-0">
+            <a
+              href="#!"
+              class="
+                col-6
+                hover-bg-gray
+                text-center
+                p-4
+                border-bottom border-end
+              "
+            >
+              Spreads
+              <!-- <img
+                        src="assets/media/brand-logos/slack.svg"
+                        class="height-30 w-auto"
+                        alt=""
+                      /> -->
+            </a>
+            <a
+              href="#!"
+              class="
+                col-6
+                hover-bg-gray
+                text-center
+                p-4
+                border-bottom border-end
+              "
+            >
+              Ticker
+            </a>
+            <a href="#!" class="col-6 hover-bg-gray text-center p-4 border-end">
+              <img
+                src="assets/media/brand-logos/atlassian.svg"
+                class="height-30 w-auto"
+                alt=""
+              />
+            </a>
+            <a href="#!" class="col-6 hover-bg-gray text-center p-4">
+              <img
+                src="assets/media/brand-logos/figma.svg"
+                class="height-30 w-auto"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <ul class="navbar-nav ms-auto d-flex align-items-center h-100">
+      <li
+        class="
+          nav-item
+          dropdown
+          d-flex
+          align-items-center
+          justify-content-center
+          flex-column
+          h-100
+          me-lg-1
+        "
+      ></li>
+    </ul>
+  </header>
+  <!--Main Header End-->
+</template>
