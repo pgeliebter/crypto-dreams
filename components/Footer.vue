245ce810bfa8905474052316788ac1400faa26d2
Pinny Geliebter <pgibralter@gmail.com>
2021-10-21 10:15:09 -0400

    adds Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="pt-4">
+    <div class="container-fluid px-7">
+      <div class="d-block lh-sm small text-muted text-end">Pinny Geliebter</div>
+    </div>
+  </footer>
+</template>
