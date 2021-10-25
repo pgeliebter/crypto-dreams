aa0925c6b25a8610510e6d35aa8b18b0421475ad
Pinny Geliebter <pgibralter@gmail.com>
2021-10-25 19:25:14 -0400

    removes redundant functions and loops in chart

--- components/ApexBarChart.vue
+++ components/ApexBarChart.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- <Themefile /> -->
     <div :id="id"></div>
   </div>
 </template>
@@ -13,25 +12,48 @@ export default {
   },
 
   mounted: function () {
-    const cee = this.dataProps.orderBooks.map((e) => e.orderBook)
-
+    // call the chart on component load
     this.ApexBarChart(this.dataProps)
   },
   props: { dataProps: Object, id: String },
   methods: {
     ApexBarChart: function (dataProps) {
+      // func to return how many digits are in the price of the crypto
       const log10 = function (n) {
         return Math.log(n) / Math.log(10)
       }
+
+      //  single loop to find the largest spread, min bid value, and max bid value to set y axis data later on
+      let largestDifference = -1
+      let minValue = dataProps.orderBooks[0].orderBook.bids[0].price
+      let maxValue = dataProps.orderBooks[0].orderBook.asks[0].price
+      // looping through data
+      for (let i = 0; i < dataProps.orderBooks.length; i++) {
+        // finding the largest spread
+        let currentDifference =
+          dataProps.orderBooks[i].orderBook.asks[0].price -
+          dataProps.orderBooks[i].orderBook.bids[0].price
+        if (currentDifference > largestDifference) {
+          largestDifference = currentDifference
+        }
+        // finding the min bid value
+        if (dataProps.orderBooks[i].orderBook.bids[0].price < minValue) {
+          minValue = dataProps.orderBooks[i].orderBook.bids[0].price
+        }
+        if (dataProps.orderBooks[i].orderBook.asks[0].price > maxValue) {
+          maxValue = dataProps.orderBooks[i].orderBook.asks[0].price
+        }
+      }
+      console.log(largestDifference, minValue, maxValue)
+
+      // set up colors for chart
       const cPrimary = '#5252F9'
       const cWarning = '#ffb016'
       const cSecondary = '#ff4d62'
-      const cSuccess = '#4BC98D'
       const cMuted = '#a5a5bc'
-      const cBodycolor = '#31314d'
-      const cLight = '#f3f6fb'
-      const cGray = '#e3e7ef'
       const cFont = 'inherit'
+
+      // new chart being auto inserted into its own named div so no conflict with any other chart
       new ApexCharts(document.querySelector(`#${this.id}`), {
         chart: {
           fontFamily: cFont,
@@ -41,6 +63,7 @@ export default {
             show: false,
           },
           animations: {
+            // animations on reload of component to make it prettier
             enabled: true,
             speed: 1000,
             animateGradually: {
@@ -75,6 +98,7 @@ export default {
           },
         },
         series: [
+          // each label gets two data one for the bids and one for ask
           {
             name: 'Bids',
             data: dataProps.orderBooks.map((x) => x.orderBook.bids[0].price),
@@ -100,6 +124,7 @@ export default {
           tooltip: {
             enabled: false,
           },
+          // catagories are our labels for the x axis
           categories: dataProps.orderBooks.map((x) => x.exchange),
           crosshairs: {
             show: false,
@@ -121,6 +146,7 @@ export default {
               colors: cMuted,
               fontFamily: cFont,
             },
+            // the formatter is using the log10 func to determine how many digits to show in the y access
             formatter: function (val, index) {
               if (log10(val) > 1) {
                 return Math.round(val)
@@ -132,45 +158,28 @@ export default {
           crosshairs: {
             show: false,
           },
+          // force nice scale allows the chart to dynamically adjust the y axis based on our previous formatter
           forceNiceScale: true,
+          // min and max are set to the min and max of our y axis
           min: function () {
-            //  function to return the largest spread to calculate min later value
-            function largestSpread(dataProps) {
-              let largestDifference = -1
-              for (let i = 0; i < dataProps.orderBooks.length; i++) {
-                let currentDifference =
-                  dataProps.orderBooks[i].orderBook.asks[0].price -
-                  dataProps.orderBooks[i].orderBook.bids[0].price
-                if (currentDifference > largestDifference) {
-                  largestDifference = currentDifference
-                }
-              }
-              return largestDifference
-            }
-            // map to get min value
-            let array = dataProps.orderBooks.map(
-              (x) => x.orderBook.bids[0].price
-            )
-            let min = Math.min(...array)
+            // using minValue from above
+            let min = minValue
             // subtracting largest spread from smallest spread to set the min value
-            min = min - largestSpread(dataProps)
-
+            min = min - largestDifference
+            // evaluating how many thousands are in our min
             if (log10(min) > 2) {
-              min = Math.round(min)
               return min
             } else {
               return min
             }
           },
           max: function () {
-            // map to get max value
-            let array = dataProps.orderBooks.map(
-              (x) => x.orderBook.asks[0].price
-            )
-            let max = Math.max(...array)
+            // using maxValue from above
+            let max = maxValue
+            // evaluating how many thousands are in our min
+
             if (log10(max) > 2) {
-              max = Math.round(max)
-              return max
+              return Math.round(max)
             } else {
               return max
             }
@@ -194,11 +203,12 @@ export default {
           intersect: false,
           inverseOrder: true,
           y: [
+            // setting up the formatter for our tool tip to show our currency and formatted price for asks and bids
             {
               formatter: function (y) {
                 if (typeof y !== 'undefined' && log10(y) <= 2) {
                   return `${dataProps.quoteSymbol} ${y.toFixed(2)}`
-                } else if (typeof y !== 'undefined4') {
+                } else if (typeof y !== 'undefined') {
                   return `${dataProps.quoteSymbol} ${y.toFixed(0)}`
                 }
                 return y
@@ -208,7 +218,7 @@ export default {
               formatter: function (y) {
                 if (typeof y !== 'undefined' && log10(y) <= 2) {
                   return `${dataProps.quoteSymbol} ${y.toFixed(2)}`
-                } else if (typeof y !== 'undefined4') {
+                } else if (typeof y !== 'undefined') {
                   return `${dataProps.quoteSymbol} ${y.toFixed(0)}`
                 }
                 return y
